565ad67f3e3d8535490b11122943b8d019ba7ed0
Q4OS Team <info@q4os.org>
2023-04-01 21:24:10 +0200

    Q4OS 5 Aquarius announcement. Installers update. Windows setup update.

--- .dev/compile_web.sh
+++ .dev/compile_web.sh
@@ -25,10 +25,3 @@ do
   OUTFILE="$INFILE"
   csstidy $INFILE $OUTFILE
 done
-
-# # add into index.html, backward compatibility for auto-downloads capability
-# echo "<!--" >> index.html
-# echo '<A HREF="downloads/setup_vboxgutils-q4_0.1.1-r3_all.esh">' >> index.html
-# echo '<A HREF="downloads/setup_q4os-chrome_0.3.3-r1_all.esh">' >> index.html
-# echo '<A HREF="downloads/setup_lookswitcher_0.3.3-r1_all.esh">' >> index.html
-# echo "-->" >> index.html
